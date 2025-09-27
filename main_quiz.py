95f477ea716896713463680d3cf8095291ce1c64
Aziz-coder-cell <abdulaziz018374@gmail.com>
2025-09-28 00:55:01 +0530

    Update main_quiz.py

--- main_quiz.py
+++ main_quiz.py
@@ -16,7 +16,7 @@ if not os.path.exists(nltk_data_dir):
 
 nltk.data.path.insert(0, nltk_data_dir)
 
-nltk.download("punkt", download_dir=nltk_data_dir)
+nltk.download("punkt_tab", download_dir=nltk_data_dir)
 nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_dir)
 
 @st.cache_data
